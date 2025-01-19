8c6ab70508166be8bd73550f7ad17dede9b087a8
ankushekapure <asekapure@gmail.com>
2025-01-19 19:01:35 +0530

    New Update

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -17,18 +17,36 @@ function Experience() {
         </h2></div>
       </div>
       <div class="experience-card">
-        <div class="ribbon">2+ years of experience</div>
-        <div class="experience-title">Software Engineer</div>
-        <div class="experience-details">
-          <p><span class="details-label">Company:</span> Snovaspace Information Systems Pvt. Ltd.</p>
-          <p><span class="details-label">Location:</span> Hyderabad, India</p>
-          <p><span class="details-label">Duration:</span> Oct 2021 - Present</p>
+        <div class="ribbon">4+ years of experience</div>
+        <div class="experience-title">Frontend Engineer</div>
+        <div class="experience-details" >
+          <p><span class="details-label">Company:</span> Eligarf Technologies Private Limited</p>
+          <p><span class="details-label">Location:</span> Pune, India</p>
+          <p><span class="details-label">Duration:</span> Dec 2023 - Present</p>
         </div>
         <div class="skills">
           <i class="fab fa-html5"></i>
           <i class="fab fa-css3-alt"></i>
           <i class="fab fa-js"></i>
           <i class="fab fa-react"></i>
+          <i class="fab fa-node"></i>
+        </div>
+
+        {/* Divider */}
+        <div style={{ height: 2, width: '100%', backgroundColor: 'white', marginTop: 30 }} />
+
+        <div class="experience-title">Software Engineer</div>
+        <div class="experience-details" >
+          <p><span class="details-label">Company:</span> Connate Software Solutions</p>
+          <p><span class="details-label">Location:</span> Hyderabad, India</p>
+          <p><span class="details-label">Duration:</span> Oct 2020 - Dec 2023</p>
+        </div>
+        <div class="skills">
+          <i class="fab fa-html5"></i>
+          <i class="fab fa-css3-alt"></i>
+          <i class="fab fa-js"></i>
+          <i class="fab fa-react"></i>
+          <i class="fab fa-node"></i>
         </div>
       </div>
     </div>
